80682bc54a69ae01dc5483565d1fabc81cc6e8a1
patryk <49292045+patryksztandera@users.noreply.github.com>
2020-07-10 15:45:30 +0200

    Initial commit

---
+++ src/main/java/pl/euvic/SportsCenterApplication.java
@@ -0,0 +1,13 @@
+package pl.euvic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/euvic/SportsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.euvic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
